e93540d865637b2ede336be04f4be4121f75bb3a
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-03-18 14:04:27 +0100

    tech stack css djust

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -7,15 +7,16 @@ function TechStack() {
   return (
     <section className="bg-light" id="tech">
       <div className="tech-stack container">
-        <div className="tech-stack-header">
-          <h1>Technologies I Use</h1>
-        </div>
-
-        <div className="tech-stack-text">
-          <p>
-            Here is the techology that I already know and also starting to
-            learn.
-          </p>
+        <div className="tech-info">
+          <div className="tech-stack-header">
+            <h1>Technologies I Use</h1>
+          </div>
+          <div className="tech-stack-text">
+            <p>
+              Here is the techology that I already know and also starting to
+              learn.
+            </p>
+          </div>
         </div>
 
         <div className="tech-stack-cards" id="tech-stack-cards">
