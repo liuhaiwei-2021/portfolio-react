ee03ee392a546181d54fcfaa5054d7e100cad24b
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-03-18 09:55:48 +0100

    projects section css adjust

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,15 +7,17 @@ function Projects() {
   return (
     <section className="bg-dark" id="projects">
       <div className="projects container">
-        <div className="projects-header">
-          <h1>Projects List</h1>
-        </div>
+        <div className="projects-info">
+          <div className="projects-header">
+            <h1>Projects List</h1>
+          </div>
 
-        <div className="projects-text">
-          <p>
-            I like to make useful and excellent products. Here are some of my
-            projects in Novare potential.
-          </p>
+          <div className="projects-text">
+            <p>
+              I like to make useful and excellent products. Here are some of my
+              projects in Novare potential.
+            </p>
+          </div>
         </div>
 
         <div className="projects-cards" id="projects-cards">
