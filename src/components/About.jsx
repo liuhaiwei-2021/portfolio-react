350bd8b1455e17729667e52e40f4c7166999c19c
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-07-17 20:40:08 +0200

    fix typo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,40 +2,40 @@
 import "../styles/About.css";
 
 function About() {
-  return (
-    <section className="bg-light" id="about">
-      <div className="about container">
-        <div className="about-info">
-          <div className="about-header">
-            <h1>About Haiwei</h1>
-          </div>
+	return (
+		<section className="bg-light" id="about">
+			<div className="about container">
+				<div className="about-info">
+					<div className="about-header">
+						<h1>About Haiwei</h1>
+					</div>
 
-          <div className="about-text">
-            <p>
-              Hi! I'm Haiwei, grew up in China , a frontend developer from
-              Stockholm Sweden.
-            </p>
-            <p>
-              Foucus on frontend skills and enjoy creating delightful and
-              hunman-centered experiences!
-            </p>
-            <p>
-              I love walking, reading, listening music, learning new things and
-              really love coding !
-            </p>
-          </div>
-        </div>
+					<div className="about-text">
+						<p>
+							Hi! I'm Haiwei, grew up in China , a frontend developer from Stockholm
+							Sweden.
+						</p>
+						<p>
+							Foucus on frontend skills and enjoy creating delightful and
+							human-centered experiences!
+						</p>
+						<p>
+							I love walking, reading, listening music, learning new things and really
+							love coding !
+						</p>
+					</div>
+				</div>
 
-        <div className="about-illustartion">
-          <img
-            className="img"
-            src="images/illustration.png"
-            alt="Woman Coder illustrtion"
-          />
-        </div>
-      </div>
-    </section>
-  );
+				<div className="about-illustartion">
+					<img
+						className="img"
+						src="images/illustration.png"
+						alt="Woman Coder illustrtion"
+					/>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default About;
