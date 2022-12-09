c565ea445370cad215ef14a2b990da228c30aea1
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 21:44:01 +0100

    reduce last line

--- src/components/CardTech.jsx
+++ src/components/CardTech.jsx
@@ -1,18 +1,16 @@
 //Projects files
 import "../styles/CardTech.css";
 
-function CardTech({ item }) {
-  const { imageURL, title } = item;
-  return (
-    <div className="card-tech">
-      <div className="card">
-        <img src={imageURL} alt={title} />
-        <div className="card-body">
-          <h3 className="card-text">{title}</h3>
-        </div>
-      </div>
-    </div>
-  );
+export default function CardTech({ item }) {
+	const { imageURL, title } = item;
+	return (
+		<div className="card-tech">
+			<div className="card">
+				<img src={imageURL} alt={title} />
+				<div className="card-body">
+					<h3 className="card-text">{title}</h3>
+				</div>
+			</div>
+		</div>
+	);
 }
-
-export default CardTech;
