c3ed3a13cdfb672818ef98f5a67191f4d82e3fc9
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 22:13:09 +0100

    remove Footer nesting code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,17 @@ import social from "../data/social.json";
 import "../styles/Footer.css";
 
 export default function Footer() {
+	const Social = social.map((item, index) => (
+		<div className="social-icon" key={index}>
+			<a href={item.socialURL}>
+				<img src={item.imageURL} alt={item.title} />
+			</a>
+		</div>
+	));
+
 	return (
 		<div className="footer">
-			<div className="social-icon-group">
-				{social.map((item, index) => (
-					<div className="social-icon" key={index}>
-						<a href={item.socialURL}>
-							<img src={item.imageURL} alt={item.title} />
-						</a>
-					</div>
-				))}
-			</div>
+			<div className="social-icon-group">{Social}</div>
 			<div className="footer-text">
 				<p>© 2022 | Designed and built by Haiwei Liu</p>
 			</div>
