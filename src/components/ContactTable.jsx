b5d197fab3a0a7e952f3b3e382e8bf18857617cc
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 21:44:54 +0100

    reduce last line

--- src/components/ContactTable.jsx
+++ src/components/ContactTable.jsx
@@ -1,22 +1,20 @@
 //Projects files
 import contact from "../data/contact.json";
 
-function ContactTable() {
-  return (
-    <table className="contact-table">
-      <tbody>
-        {contact.map((item, index) => (
-          <tr key={index}>
-            <td>{item.title}</td>
-            <td>
-              <img src={item.imageURL} alt={item.title} />
-              {item.content}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+export default function ContactTable() {
+	return (
+		<table className="contact-table">
+			<tbody>
+				{contact.map((item, index) => (
+					<tr key={index}>
+						<td>{item.title}</td>
+						<td>
+							<img src={item.imageURL} alt={item.title} />
+							{item.content}
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 }
-
-export default ContactTable;
