182650249d8610bb32f51c5acb5d0922480c27f3
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 21:46:04 +0100

    reduce last line

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,46 +1,44 @@
 //Project files
 import "../styles/Navigation.css";
 
-function Navigation() {
-  return (
-    <div className="nav-bar">
-      <div className="nav-container">
-        <div>
-          <a className="navbar-link" href="#hero">
-            <img className="logo" src="images/logo-haiwei.png" alt="logo" />
-          </a>
-        </div>
+export default function Navigation() {
+	return (
+		<div className="nav-bar">
+			<div className="nav-container">
+				<div>
+					<a className="navbar-link" href="#hero">
+						<img className="logo" src="images/logo-haiwei.png" alt="logo" />
+					</a>
+				</div>
 
-        <ul className="navbar-items">
-          <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="#hero">
-              Hero
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#about">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#projects">
-              Projects
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#tech">
-              Tech Stack
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#contact">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+				<ul className="navbar-items">
+					<li className="nav-item">
+						<a className="nav-link" aria-current="page" href="#hero">
+							Hero
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#about">
+							About
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#projects">
+							Projects
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#tech">
+							Tech Stack
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#contact">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
-
-export default Navigation;
