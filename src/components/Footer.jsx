0b6835e10910da6c564233f037d7415416827b05
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 21:45:17 +0100

    reduce last line

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,23 +2,21 @@
 import social from "../data/social.json";
 import "../styles/Footer.css";
 
-function Footer() {
-  return (
-    <div className="footer">
-      <div className="social-icon-group">
-        {social.map((item, index) => (
-          <div className="social-icon" key={index}>
-            <a href={item.socialURL}>
-              <img src={item.imageURL} alt={item.title} />
-            </a>
-          </div>
-        ))}
-      </div>
-      <div className="footer-text">
-        <p>© 2022 | Designed and built by Haiwei Liu</p>
-      </div>
-    </div>
-  );
+export default function Footer() {
+	return (
+		<div className="footer">
+			<div className="social-icon-group">
+				{social.map((item, index) => (
+					<div className="social-icon" key={index}>
+						<a href={item.socialURL}>
+							<img src={item.imageURL} alt={item.title} />
+						</a>
+					</div>
+				))}
+			</div>
+			<div className="footer-text">
+				<p>© 2022 | Designed and built by Haiwei Liu</p>
+			</div>
+		</div>
+	);
 }
-
-export default Footer;
