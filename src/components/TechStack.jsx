3fb0f69610642828aea661ba0f6924e108a31ae4
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 21:46:47 +0100

    reduce last line

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,29 +3,23 @@ import tech from "../data/tech.json";
 import "../styles/TechStack.css";
 import CardTech from "./CardTech";
 
-function TechStack() {
-  return (
-    <section className="bg-light" id="tech">
-      <div className="tech-stack container">
-        <div className="tech-info">
-          <div className="tech-stack-header">
-            <h1>Technologies I Use</h1>
-          </div>
-          <div className="tech-stack-text">
-            <p>
-              Here is the techology that I already know and also starting to
-              learn.
-            </p>
-          </div>
-        </div>
+export default function TechStack() {
+	return (
+		<section className="bg-light" id="tech">
+			<div className="tech-stack container">
+				<div className="tech-info">
+					<div className="tech-stack-header">
+						<h1>Technologies I Use</h1>
+					</div>
+					<div className="tech-stack-text">
+						<p>Here is the techology that I already know and also starting to learn.</p>
+					</div>
+				</div>
 
-        <div className="tech-stack-cards" id="tech-stack-cards">
-          {tech.length > 0 &&
-            tech.map((item) => <CardTech key={item.id} item={item} />)}
-        </div>
-      </div>
-    </section>
-  );
+				<div className="tech-stack-cards" id="tech-stack-cards">
+					{tech.length > 0 && tech.map((item) => <CardTech key={item.id} item={item} />)}
+				</div>
+			</div>
+		</section>
+	);
 }
-
-export default TechStack;
