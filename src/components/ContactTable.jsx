ebac3235f7f28c863a8d77dd8f90167af6e14b03
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 22:09:09 +0100

    remove ContactTable nesting code

--- src/components/ContactTable.jsx
+++ src/components/ContactTable.jsx
@@ -2,19 +2,19 @@
 import contact from "../data/contact.json";
 
 export default function ContactTable() {
+	const Rows = contact.map((item, index) => (
+		<tr key={index}>
+			<td>{item.title}</td>
+			<td>
+				<img src={item.imageURL} alt={item.title} />
+				{item.content}
+			</td>
+		</tr>
+	));
+
 	return (
 		<table className="contact-table">
-			<tbody>
-				{contact.map((item, index) => (
-					<tr key={index}>
-						<td>{item.title}</td>
-						<td>
-							<img src={item.imageURL} alt={item.title} />
-							{item.content}
-						</td>
-					</tr>
-				))}
-			</tbody>
+			<tbody>{Rows}</tbody>
 		</table>
 	);
 }
