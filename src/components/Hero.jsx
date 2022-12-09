c502f53d763155fb0673f60a089dc6ca2d5c1f9b
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 21:45:37 +0100

    reduce last line

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,14 @@
 //Project files
 import "../styles/Hero.css";
 
-function Hero() {
-  return (
-    <section className="hero" id="hero">
-      <img className="hero-image" src="/images/hero.png" />
-      <div className="hero-text">
-        <h1>Hi There, I am Haiwei Liu</h1>
-        <h3>And I'm a Frontend Developer!</h3>
-      </div>
-    </section>
-  );
+export default function Hero() {
+	return (
+		<section className="hero" id="hero">
+			<img className="hero-image" src="/images/hero.png" />
+			<div className="hero-text">
+				<h1>Hi There, I am Haiwei Liu</h1>
+				<h3>And I'm a Frontend Developer!</h3>
+			</div>
+		</section>
+	);
 }
-
-export default Hero;
