b5ccb5b8c96dcb70cf01c3761ad32bb79c95b751
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-09 21:44:28 +0100

    reduce last line

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,17 +2,15 @@
 import ContactTable from "./ContactTable";
 import "../styles/Contact.css";
 
-function Contact() {
-  return (
-    <div className="bg-dark" id="contact">
-      <div className="contact container">
-        <div className="contact-info">
-          <h1>Contact</h1>
-        </div>
-        <ContactTable />
-      </div>
-    </div>
-  );
+export default function Contact() {
+	return (
+		<div className="bg-dark" id="contact">
+			<div className="contact container">
+				<div className="contact-info">
+					<h1>Contact</h1>
+				</div>
+				<ContactTable />
+			</div>
+		</div>
+	);
 }
-
-export default Contact;
