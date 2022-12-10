f9fcfeeb6d5357b9768a0c88322940dcf176988d
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-10 22:47:09 +0100

    about css refactor

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,33 +5,22 @@ export default function About() {
 	return (
 		<section className="bg-light" id="about">
 			<div className="about container">
+				<h1 className="about-header">About Haiwei</h1>
 				<div className="about-info">
-					<div className="about-header">
-						<h1>About Haiwei</h1>
-					</div>
+					<p className="about-paragraph">
+						Hi! I'm Haiwei, grew up in China , a frontend developer from Stockholm
+						Sweden. Focus on frontend skills and enjoy creating delightful and
+						human-centred experiences! I love walking, reading, listening music,
+						learning new things and really love coding !
+					</p>
 
-					<div className="about-text">
-						<p>
-							Hi! I'm Haiwei, grew up in China , a frontend developer from Stockholm
-							Sweden.
-						</p>
-						<p>
-							Focus on frontend skills and enjoy creating delightful and human-centred
-							experiences!
-						</p>
-						<p>
-							I love walking, reading, listening music, learning new things and really
-							love coding !
-						</p>
+					<div className="about-illustartion">
+						<img
+							className="img"
+							src="images/illustration.png"
+							alt="Woman Coder illustrtion"
+						/>
 					</div>
-				</div>
-
-				<div className="about-illustartion">
-					<img
-						className="img"
-						src="images/illustration.png"
-						alt="Woman Coder illustrtion"
-					/>
 				</div>
 			</div>
 		</section>
